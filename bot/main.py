8707bae2d0a8b8418b67a4c95e8a0138bd611429
OldestSalt <personal@oldestsalt.ru>
2026-01-14 00:44:54 +1000

    sending to queue

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,8 @@ import logging
 import os
 import aioboto3
 from minio import Minio
+import pika
+import json
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,7 +19,15 @@ logger = logging.getLogger(__name__)
 load_dotenv("../.env")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+IMG_BUCKET_NAME = os.getenv("IMG_BUCKET_NAME")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
 DEV = os.getenv("DEV")
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host="localhost" if DEV else "rabbitmq", port=RABBITMQ_PORT)
+)
+channel = connection.channel()
+channel.exchange_declare(exchange="images", exchange_type="fanout")
 
 minio_client = Minio(
     endpoint="http://localhost:9000" if DEV else "http://minio:9000",
@@ -27,13 +37,13 @@ minio_client = Minio(
 
 async def upload_file(path):
     session = aioboto3.Session()
-    async with session.Client(
+    async with session.client(
         "s3",
         endpoint_url="http://localhost:9000" if DEV else "http://minio:9000",
         aws_access_key_id=os.getenv("MINIO_ROOT_USER"),
         aws_secret_access_key=os.getenv("MINIO_ROOT_PASSWORD")
     ) as client:
-        await client.upload_file(path, "memes", path)
+        await client.upload_file(path, IMG_BUCKET_NAME, path)
 
 async def log_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     photo = update.effective_message.photo if update.effective_message.photo else None
@@ -41,20 +51,28 @@ async def log_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
         photo_file = await photo[-1].get_file()
         img = await photo_file.download_to_drive("./temp/")
         await upload_file(img)
-        # add to rabbitmq queue
+        img_name = os.path.split(img)[1]
+        img.unlink()
+        img_json = json.dumps({"img_name": img_name, "bucket": IMG_BUCKET_NAME})
+        channel.basic_publish(exchange="images", routing_key="", body=img_json)
 
 
 def main():
-    if BOT_TOKEN is None:
-        raise ValueError("Bot token is not defined")
-    if not os.path.exists("./temp"):
-        os.mkdir("./temp")
-    if not minio_client.bucket_exists("memes"):
-        minio_client.make_bucket("memes")
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(MessageHandler(~filters.FORWARDED, log_message))
+    try:
+        if BOT_TOKEN is None:
+            raise ValueError("Bot token is not defined")
+        if not os.path.exists("./temp"):
+            os.mkdir("./temp")
+        if not minio_client.bucket_exists(IMG_BUCKET_NAME):
+            minio_client.make_bucket(IMG_BUCKET_NAME)
+        application = Application.builder().token(BOT_TOKEN).build()
+        application.add_handler(MessageHandler(~filters.FORWARDED, log_message))
 
-    application.run_polling(allowed_updates=Update.CHANNEL_POST)
+        application.run_polling(allowed_updates=Update.CHANNEL_POST)
+    except Exception as e:
+        logger.error(e)
+        connection.close()
+        raise e
 
 if __name__ == '__main__':
     main()
